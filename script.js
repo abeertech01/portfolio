4f55e0552c27556a254caf27e752e9aa38c16d66
Abeer <abeer.technology@gmail.com>
2021-06-27 11:10:28 +0600

    fixed scrolling and nav menu hiding

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Show Menu
+//////// Show Menu /////////////
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId);
   const navMenu = document.getElementById(navId);
@@ -10,3 +10,27 @@ const showMenu = (toggleId, navId) => {
   }
 }
 showMenu('menuToggler', 'navMenu');
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.header__nav-link');
+
+function linkAction() {
+  // Active Link
+  navLink.forEach(n => n.classList.remove('active'));
+
+  // Remove menu mobile
+  const navMenu = document.getElementById('navMenu');
+  navMenu.classList.remove('show');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/////////// Closing Nav Menu When Click Outside///////////
+const sections = document.querySelectorAll('.out-menu');
+
+sections.forEach(s => {
+  s.addEventListener('click', () => {
+    // Remove menu mobile
+    const navMenu = document.getElementById('navMenu');
+    navMenu.classList.remove('show');
+  })
+})
